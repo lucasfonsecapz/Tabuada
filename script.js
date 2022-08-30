1c89d95367abe984635b561cb0091e5e09e21e0c
Lucas Fonseca <110259302+lucasfonsecapz@users.noreply.github.com>
2022-08-30 14:15:46 -0300

    update

---
+++ script.js
@@ -0,0 +1,18 @@
+function Tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value == 0) {
+        window.alert('Por favor, digite um número')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+
+        for (let c = 1; c <= 10; c++) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n* c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
+}
+   
